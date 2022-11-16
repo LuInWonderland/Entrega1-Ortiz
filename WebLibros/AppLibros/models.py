aecc4e413069d3c4f1b5dd9db33f9462dec6781f
Franco Muzzio <48494284+FrancoMuzzio@users.noreply.github.com>
2022-11-16 20:52:29 -0300

    cosas q hice en la compu de fran

--- WebLibros/AppLibros/models.py
+++ WebLibros/AppLibros/models.py
@@ -4,24 +4,24 @@ from django.db import models
 class CienciaFiccion(models.Model):
     titulo = models.CharField(max_length=40)
     autor  = models.CharField(max_length=40)
-    sinopsis = models.CharField(max_length=255)
-    url_portada = models.CharField(max_length=40)
+    sinopsis = models.CharField(max_length=1000)
+    url_portada = models.CharField(max_length=255)
 
 class Terror(models.Model):
     titulo = models.CharField(max_length=40)
     autor  = models.CharField(max_length=40)
-    sinopsis = models.CharField(max_length=255)
-    url_portada = models.CharField(max_length=40)
+    sinopsis = models.CharField(max_length=1000)
+    url_portada = models.CharField(max_length=255)
 
 class Distopia(models.Model):
     titulo = models.CharField(max_length=40)
     autor  = models.CharField(max_length=40)
-    sinopsis = models.CharField(max_length=255)
-    url_portada = models.CharField(max_length=40)
+    sinopsis = models.CharField(max_length=1000)
+    url_portada = models.CharField(max_length=255)
 
 class Fantasia(models.Model):
     titulo = models.CharField(max_length=40)
     autor  = models.CharField(max_length=40)
-    sinopsis = models.CharField(max_length=255)
-    url_portada = models.CharField(max_length=40)
+    sinopsis = models.CharField(max_length=1000)
+    url_portada = models.CharField(max_length=255)
 
--- WebLibros/AppLibros/views.py
+++ WebLibros/AppLibros/views.py
@@ -12,29 +12,58 @@ def inicio(request):
     return HttpResponse(documento)
 
 def cienciaFiccion(request):
-    plantilla = loader.get_template('cienciaFiccion.html')
-    contexto = {}
-    documento = plantilla.render(contexto)
-    return HttpResponse(documento)
+    form = forms.CienciaFiccionForm()
+    libros = models.CienciaFiccion.objects.all()
+    if request.method == 'POST':
+        form = forms.CienciaFiccionForm(request.POST)
+        if form.is_valid():
+            info = form.cleaned_data
+            libro_ciencia_ficcion = models.CienciaFiccion(titulo = info['titulo'], autor = info['autor'], sinopsis = info['sinopsis'], url_portada = info['url_portada'])
+            libro_ciencia_ficcion.save()
+    if request.method == 'GET' and 'buscador' in request.GET:
+        buscador = request.GET['buscador']
+        libros = models.CienciaFiccion.objects.filter(titulo__icontains=buscador).union(models.CienciaFiccion.objects.filter(autor__icontains=buscador))
+    return render(request, 'cienciaFiccion.html', {'form': form, 'libros': libros})
+
+def distopia(request):
+    form = forms.DistopiaForm()
+    libros = models.Distopia.objects.all()
+    if request.method == 'POST':
+        form = forms.DistopiaForm(request.POST)
+        if form.is_valid():
+            info = form.cleaned_data
+            libro_distopia = models.Distopia(titulo = info['titulo'], autor = info['autor'], sinopsis = info['sinopsis'], url_portada = info['url_portada'])
+            libro_distopia.save()
+    if request.method == 'GET' and 'buscador' in request.GET:
+        buscador = request.GET['buscador']
+        libros = models.Distopia.objects.filter(titulo__icontains=buscador).union(models.Distopia.objects.filter(autor__icontains=buscador))
+    return render(request, 'distopia.html', {'form': form, 'libros': libros})
+
 
 def terror(request):
+    form = forms.TerrorForm()
+    libros = models.Terror.objects.all()
     if request.method == 'POST':
         form = forms.TerrorForm(request.POST)
         if form.is_valid():
             info = form.cleaned_data
             libro_terror = models.Terror(titulo = info['titulo'], autor = info['autor'], sinopsis = info['sinopsis'], url_portada = info['url_portada'])
             libro_terror.save()
-    form = forms.TerrorForm()
-    return render(request, 'terror.html', {'form': form})
-
-def distopia(request):
-    plantilla = loader.get_template('distopia.html')
-    contexto = {}
-    documento = plantilla.render(contexto)
-    return HttpResponse(documento)
+    if request.method == 'GET' and 'buscador' in request.GET:
+        buscador = request.GET['buscador']
+        libros = models.Terror.objects.filter(titulo__icontains=buscador).union(models.Terror.objects.filter(autor__icontains=buscador))
+    return render(request, 'terror.html', {'form': form, 'libros': libros})
 
 def fantasia(request):
-    plantilla = loader.get_template('fantasia.html')
-    contexto = {}
-    documento = plantilla.render(contexto)
-    return HttpResponse(documento)
+    form = forms.FantasiaForm()
+    libros = models.Fantasia.objects.all()
+    if request.method == 'POST':
+        form = forms.FantasiaForm(request.POST)
+        if form.is_valid():
+            info = form.cleaned_data
+            libro_fantasia = models.Fantasia(titulo = info['titulo'], autor = info['autor'], sinopsis = info['sinopsis'], url_portada = info['url_portada'])
+            libro_fantasia.save()
+    if request.method == 'GET' and 'buscador' in request.GET:
+        buscador = request.GET['buscador']
+        libros = models.Fantasia.objects.filter(titulo__icontains=buscador).union(models.Fantasia.objects.filter(autor__icontains=buscador))
+    return render(request, 'fantasia.html', {'form': form, 'libros': libros})
